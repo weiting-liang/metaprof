21ad1532baed925d090c438c7f11350b42c59507
liangweiting <76460001+weiting-liang@users.noreply.github.com>
2022-06-02 11:09:42 +0800

    Update snakemake.sh

--- snakemake.sh
+++ snakemake.sh
@@ -7,5 +7,5 @@ snakemake \
 --jobs 50 \
 --keep-going \
 --rerun-incomplete \
---latency-wait 360 
+--latency-wait 360 \
 --cluster "qsub -S /bin/bash -cwd -q {cluster.queue} -P {cluster.project} -l vf={cluster.mem},p={cluster.cores} -binding linear:{cluster.cores} -o {cluster.output} -e {cluster.error}"
